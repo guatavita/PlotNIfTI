f4c18f5865982123a195e38698bbc359a779c732
unknown <bastien.rig@gmail.com>
2022-03-17 15:27:30 +0100

    change main

--- main.py
+++ main.py
@@ -9,16 +9,27 @@
 from PlotNIfTI import PlotNifti
 
 def main():
-    image_path = r"C:\Data\Data_test\plot_cervix\image.nii.gz"
-    segmentation_paths = [r"C:\Data\Data_test\plot_cervix\CTVT.nii.gz",
-                          r"C:\Data\Data_test\plot_cervix\Bladder.nii.gz",
-                          r"C:\Data\Data_test\plot_cervix\Rectum.nii.gz",
-                          r"C:\Data\Data_test\plot_cervix\BowelBag.nii.gz"]
+    image_path = r"C:\Data\Data_test\plot_cervix_ct\image.nii.gz"
+    segmentation_paths = [r"C:\Data\Data_test\plot_cervix_ct\CTVT.nii.gz",
+                          r"C:\Data\Data_test\plot_cervix_ct\Bladder.nii.gz",
+                          r"C:\Data\Data_test\plot_cervix_ct\Rectum.nii.gz",
+                          r"C:\Data\Data_test\plot_cervix_ct\Sigmoid.nii.gz",
+                          r"C:\Data\Data_test\plot_cervix_ct\BowelBag.nii.gz"]
 
     for view in ['axial', 'sagittal', 'coronal']:
         output_path = r"C:\Data\Data_test\plot_cervix\screenshot_{}.png".format(view)
         PlotNifti(image_path=image_path, segmentation_paths=segmentation_paths, output_path=output_path, view=view,
                   show_contour=True, show_filled=True, transparency=0.20, get_at_centroid=True)
+
+    # image_path = r"C:\Data\Data_test\plot\image.nii.gz"
+    # segmentation_paths = [r"C:\Data\Data_test\plot\Prostate.nii.gz",
+    #                       r"C:\Data\Data_test\plot\Bladder.nii.gz",
+    #                       r"C:\Data\Data_test\plot\Rectum.nii.gz"]
+    #
+    # for view in ['axial', 'sagittal', 'coronal']:
+    #     output_path = r"C:\Data\Data_test\plot\screenshot_{}.png".format(view)
+    #     PlotNifti(image_path=image_path, segmentation_paths=segmentation_paths, output_path=output_path, view=view,
+    #               show_contour=True, show_filled=True, transparency=0.20, get_at_centroid=True)
 
 if __name__ == '__main__':
     main()
