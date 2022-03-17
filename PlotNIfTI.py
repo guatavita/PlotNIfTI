0b0996db3257b2ea70206551aad419b868638cd2
unknown <bastien.rig@gmail.com>
2022-03-17 10:34:40 +0100

    add submodule and init files

---
+++ PlotNIfTI.py
@@ -0,0 +1,11 @@
+# Created by Bastien Rigaud at 17/03/2022
+# Bastien Rigaud, PhD
+# Laboratoire Traitement du Signal et de l'Image (LTSI), INSERM U1099
+# Campus de Beaulieu, Université de Rennes 1
+# 35042 Rennes, FRANCE
+# bastien.rigaud@univ-rennes1.fr
+# Description:
+
+class plot_nifti(object):
+    def __init__(self):
+        xxx=1
---
+++ main.py
@@ -0,0 +1,15 @@
+# Created by Bastien Rigaud at 17/03/2022
+# Bastien Rigaud, PhD
+# Laboratoire Traitement du Signal et de l'Image (LTSI), INSERM U1099
+# Campus de Beaulieu, Université de Rennes 1
+# 35042 Rennes, FRANCE
+# bastien.rigaud@univ-rennes1.fr
+# Description:
+
+from PlotNIfTI import plot_nifti
+
+def main():
+    plotter = plot_nifti()
+
+if __name__ == '__main__':
+    main()
