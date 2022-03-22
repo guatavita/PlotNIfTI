87de68ec2e9d2aa0c3c2a7613e5e07c9e743f04e
unknown <bastien.rig@gmail.com>
2022-03-22 14:14:52 +0100

    force matplotlib 3.5.1

--- main.py
+++ main.py
@@ -10,22 +10,40 @@ from PlotNIfTI import PlotNifti
 
 
 def main():
-    image_path = r"C:\Data\Data_test\plot_cervix_ct\image.nii.gz"
-    segmentation_paths = [r"C:\Data\Data_test\plot_cervix_ct\CTVT.nii.gz",
-                          r"C:\Data\Data_test\plot_cervix_ct\Bladder.nii.gz",
-                          r"C:\Data\Data_test\plot_cervix_ct\Rectum.nii.gz",
-                          r"C:\Data\Data_test\plot_cervix_ct\Sigmoid.nii.gz",
-                          r"C:\Data\Data_test\plot_cervix_ct\BowelBag.nii.gz"]
+    # image_path = r"C:\Data\Data_test\plot_cervix_ct\image.nii.gz"
+    # segmentation_paths = [r"C:\Data\Data_test\plot_cervix_ct\CTVT.nii.gz",
+    #                       r"C:\Data\Data_test\plot_cervix_ct\Bladder.nii.gz",
+    #                       r"C:\Data\Data_test\plot_cervix_ct\Rectum.nii.gz",
+    #                       r"C:\Data\Data_test\plot_cervix_ct\Sigmoid.nii.gz",
+    #                       r"C:\Data\Data_test\plot_cervix_ct\BowelBag.nii.gz"]
+    #
+    # views = ['sagittal', 'axial', 'coronal']
+    # # segmentation_names is optional, but useful to add a colormap
+    # segmentation_names = ['CTVT', 'Bladder', 'Rectum', 'Sigmoid', 'BowelBag']
+    # plot_object = PlotNifti(image_path=image_path, segmentation_paths=segmentation_paths,
+    #                         show_contour=True, show_filled=True, transparency=0.20, get_at_centroid=True,
+    #                         segmentation_names=segmentation_names)
+    #
+    # for view in views:
+    #     output_path = r"example\screenshot_{}.png".format(view)
+    #     plot_object.set_view(view)
+    #     plot_object.set_output_path(output_path)
+    #     plot_object.generate_plot()
 
-    views = ['sagittal', 'axial', 'coronal']
+    image_path = r"C:\Data\Data_test\plot\image.nii.gz"
+    segmentation_paths = [r"C:\Data\Data_test\plot\Prostate_DL_v0.nii.gz",
+                          r"C:\Data\Data_test\plot\Bladder_DL_v0.nii.gz",
+                          r"C:\Data\Data_test\plot\Rectum_DL_v0.nii.gz"]
+
+    views = ['sagittal']
     # segmentation_names is optional, but useful to add a colormap
-    segmentation_names = ['CTVT', 'Bladder', 'Rectum', 'Sigmoid', 'BowelBag']
+    segmentation_names = ['Prostate', 'Bladder', 'Rectum']
     plot_object = PlotNifti(image_path=image_path, segmentation_paths=segmentation_paths,
                             show_contour=True, show_filled=True, transparency=0.20, get_at_centroid=True,
                             segmentation_names=segmentation_names)
 
     for view in views:
-        output_path = r"example\screenshot_{}.png".format(view)
+        output_path = r"C:\Data\Data_test\plot\screenshot_{}.png".format(view)
         plot_object.set_view(view)
         plot_object.set_output_path(output_path)
         plot_object.generate_plot()
